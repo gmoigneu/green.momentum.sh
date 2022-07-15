ff4c6c0e38e5481227dcc819876d8ea963e03529
Guillaume Moigneu <g@moigneu.com>
2022-07-15 14:07:43 +0200

    Enable telescope #2

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -64,9 +64,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+            return true;
         });
     }
 }
